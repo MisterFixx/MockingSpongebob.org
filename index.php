5c61f1c25c1f879979533224cba79faf08d5056a
Jordan Jones <mister_fix@mc-gc.net>
2017-12-26 03:38:09 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<title>MockingSpongebob.org</title>
+Made by Mister_Fix.<br />
+<small>Hi codevision</small><br />
+<i><b>usage</b></i>: <br /><code>https://MockingSpongebob.org/your text here</code><br /><br />
+<i><b>Additionally</b></i> <br />if you need forum friendly URLs, you can replace spaces with underscores (_) or plus signs (+), and/or add a .jpg file extension at the end, it'll work just fine.<br /><br />       
