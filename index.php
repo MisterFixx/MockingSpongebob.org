eefd9e2cd5c24f696ab3ac4770b52c59825a5df2
Matt <cubedgamecompany@gmail.com>
2017-12-25 22:51:11 -0800

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,5 +0,0 @@
-<title>MockingSpongebob.org</title>
-Made by Mister_Fix.<br />
-<small>Hi codevision</small><br />
-<i><b>usage</b></i>: <br /><code>https://MockingSpongebob.org/your text here</code><br /><br />
-<i><b>Additionally</b></i> <br />if you need forum friendly URLs, you can replace spaces with underscores (_) or plus signs (+), and/or add a .jpg file extension at the end, it'll work just fine.<br /><br />       
