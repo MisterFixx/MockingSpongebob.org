f5168b09d7744f0b103e4b85bd2675fad95518cf
teevik <teemu.vikoren@gmail.com>
2017-12-28 16:10:15 +0100

    Change the markup for the new styles

--- index.php
+++ index.php
@@ -6,24 +6,19 @@
         <link rel="stylesheet" type="text/css"  href="css/main.css">
     </head>
     <body>
-    <div id="header">Mocking Spongebob</div>
-        <div id="container">
-            <div id="cont-top">
-                <p>Visit <a href="http://mockingspongebob.org/Fucking_Nigger">mockingspongebob.org/{text}</a> to view your very own mocking Spongebob.
-                <br>Additionally, you can replace spaces with "+" or "_" in order to make the URLs compatible with certain forums software or you can append ".jpg" to the end of the URL.</p>
-                <p>Add &amp;color=red,black,blue,green,white to change the text color.</p>
-                <p>Add &amp;blur to blur the background.</p>
-                <p>Add &amp;random to choose a random background image.</p>
-                
-                <br>
-                <small>#bansxtanna</small>
-            </div>
-            <div id="cont-bottom">
-                <div>&nbsp;&nbsp;<img src="/Mocking Spongebob" width="90%" id="imageSrc"></img></div>
-                <div>
-                    Text: <input type="text" id="imageText">
-                    <br>
-                    Text Color:
+        <header class="header">
+            <h1>
+                Mocking Spongebob
+            </h1>
+        </header>
+        <main class="main">
+            <div class="content">
+                <section class="image-customizer">
+                    <div><img src="http://mockingspongebob.org/Mocking_Spongebob" id="imageSrc"></img></div>
+                    <div>
+                        Text: <input type="text" id="imageText" value="mocking spongebob">
+                        <br>
+                        Text Color:
                         <select id="imageColor">
                             <option value="white">White</option>
                             <option value="black">Black</option>
@@ -31,57 +26,73 @@
                             <option value="green">Green</option>
                             <option value="blue">Blue</option>
                         </select>
-                    <br>
-                    Blur background:
+                        <br>
+                        Blur background:
                         <input type="checkbox" id="imageBlur"/>
-                    <br>
-                    Random background:
+                        <br>
+                        Random background:
                         <input type="checkbox" id="imageRandom"/>
-                    <br><br><br>
-                    <textarea type="text" readonly style="width:90%;height:50px;" id="imageSrcVal"placeholder="Please use the options above"></textarea>
-                </div>
-            
+                        <br>
+                        <br>
+                        <textarea type="text" readonly id="imageSrcVal"placeholder="Please use the options above"></textarea>
+                    </div>
+                </section>
+                <hr>
+                <section class="instructions">
+                    <p>Visit <span class="code"><a href="http://mockingspongebob.org/text">mockingspongebob.org/{text}</a></span> to view your very own mocking Spongebob.</p>
+                    <p>Additionally, you can replace spaces with <span class="code">+</span> or <span class="code">_</span> in order to make the URLs compatible with certain forums software or you can append <span class="code">.jpg</span> to the end of the URL.</p>
+                    <ul>
+                        <li>Add <span class="code">&amp;color=red, black, blue, green or white</span> to change the text color.</li>
+                        <li>Add <span class="code">&amp;blur</span> to blur the background.</li>
+                        <li>Add <span class="code">&amp;random</span> to choose a random background image.</li>
+                    </ul>
+                </section>
+                <hr>
+                <section class="credits">
+                    <p>
+                        Created by <b>Mister_Fix</b>
+                        <br>
+                        More added by <b>LeonFagan71</b>
+                    </p>
+                    <p>
+                        Images served so far: <b>1688</b>
+                    </p>
+                </section>
             </div>
-        </div>
-        <div id="botleft">
-            Created by Mister_Fix<br>More added by LeonFagan71
-        </div>
-        <div id="botright">
-                <i><b>Images served so far: 1688</b></i>
-        </div>
+        </main>
         <script type="text/javascript">
-        function updateImage(){
-            var imageText=document.getElementById("imageText").value.split(' ').join('_');
-            var imageColor=document.getElementById("imageColor").value;
-            var imageBlur=document.getElementById("imageBlur").checked;
-            var imageRandom=document.getElementById("imageRandom").checked;
-            var url="https://mockingspongebob.org/";
-            url+=imageText;
-            if (imageColor!="white")url+="&color="+imageColor;
-            if (imageBlur) url+="&blur";
-            if (imageRandom) url+="&random";
-            console.log(url);
-            var imgSrc=document.getElementById("imageSrc");
-            var imgSrcVal=document.getElementById("imageSrcVal");
-            imgSrcVal.value=url;
-            chageIcon(imgSrc, url);
-        }
-        function chageIcon(domImg,srcImage){
-            var img = new Image();
-            img.onload = function()
-            {
-                // Load completed
-                domImg.src = this.src;
-            };
-            img.src = srcImage;
-        }
+            function updateImage(){
+                var imageText=document.getElementById("imageText").value.split(' ').join('_');
+                var imageColor=document.getElementById("imageColor").value;
+                var imageBlur=document.getElementById("imageBlur").checked;
+                var imageRandom=document.getElementById("imageRandom").checked;
+                var url="https://mockingspongebob.org/";
+                url+=imageText;
+                if (imageColor!="white")url+="&color="+imageColor;
+                if (imageBlur) url+="&blur";
+                if (imageRandom) url+="&random";
+                console.log(url);
+                var imgSrc=document.getElementById("imageSrc");
+                var imgSrcVal=document.getElementById("imageSrcVal");
+                imgSrcVal.value=url;
+                chageIcon(imgSrc, url);
+            }
+            function chageIcon(domImg,srcImage){
+                var img = new Image();
+                img.onload = function()
+                {
+                    // Load completed
+                    domImg.src = this.src;
+                };
+                img.src = srcImage;
+            }
         
         
         
-        document.getElementById("imageText").addEventListener('change', updateImage);
-        document.getElementById("imageColor").addEventListener('change', updateImage);
-        document.getElementById("imageBlur").addEventListener('change', updateImage);
-        document.getElementById("imageRandom").addEventListener('change', updateImage);
+            document.getElementById("imageText").addEventListener('change', updateImage);
+            document.getElementById("imageColor").addEventListener('change', updateImage);
+            document.getElementById("imageBlur").addEventListener('change', updateImage);
+            document.getElementById("imageRandom").addEventListener('change', updateImage);
         
         </script>
     </body>
